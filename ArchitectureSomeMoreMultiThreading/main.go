b9065c14a9ffca68e6154995ccfc078a4cf53dda
Mmmr7 <ol-sidorenkov@mail.ru>
2022-01-03 04:36:37 +0300

    basic random input logic

--- ArchitectureSomeMoreMultiThreading/main.go
+++ ArchitectureSomeMoreMultiThreading/main.go
@@ -3,22 +3,24 @@ package main
 import (
 	"ArchitectureSomeMoreMultiThreading/factory"
 	"fmt"
-	"os"
-	"strconv"
+	"math/rand"
 	"time"
 )
 
 func getPins(numOfPins int, pins []*factory.Pin) {
+	pins = make([]*factory.Pin, numOfPins)
 	for i := 0; i < numOfPins; i++ {
+		pins[i] = factory.NewPin(rand.Float64(), rand.Float64())
 	}
 }
 
 func main() {
 	start := time.Now()
 
-	numOfPins, _ := strconv.Atoi(os.Args[1])
-	for i := 0; i < numOfPins; i++ {
-	}
+	//numOfPins, _ := strconv.Atoi(os.Args[1])
+	numOfPins := 10
+	var pins []*factory.Pin
+	getPins(numOfPins, pins)
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(start))
 }
